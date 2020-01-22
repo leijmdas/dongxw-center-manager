144212e23eda0a2d3d274158b0767fc0617cc0dd
leijmdas <leijmdas@163.com>
2020-01-23 02:00:59 +0800

    add

--- center-server/src/main/java/cn/kunlong/center/CenterApp.java
+++ center-server/src/main/java/cn/kunlong/center/CenterApp.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 
 @EnableAutoConfiguration
 @Configuration
