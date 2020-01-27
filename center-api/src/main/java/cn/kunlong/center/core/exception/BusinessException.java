e20048093ac011bc7a896f3676ca995a86d28eb2
leijmdas <leijmdas@163.com>
2020-01-28 00:56:53 +0800

    add

--- center-api/src/main/java/cn/kunlong/center/core/exception/BusinessException.java
+++ center-api/src/main/java/cn/kunlong/center/core/exception/BusinessException.java
@@ -8,9 +8,20 @@ public class BusinessException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	private String code;
 
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	private String msg;
+
 	public BusinessException(String code,String msg) {
 		super(msg);
 		this.code = code;
+		this.msg = msg;
 	}
 	
 	public BusinessException(String msg) {
